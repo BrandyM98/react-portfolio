e6b68c6e2c239979d1ca7d485477ee295f7079f0
Brandy Moore <brandymoore923@gmail.com>
2022-10-22 17:51:08 -0500

    added contact form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "../src/Components/Header";
 import About from "../src/Components/About";
 import Portfolio from "../src/Components/Portfolio";
+import Contact from "../src/Components/Contact";
 
 import informationData from "../src/information";
 
@@ -14,6 +15,7 @@ class App extends Component {
         <Header informationData={informationData}/>
         <About informationData={informationData}/>
         <Portfolio informationData={informationData}/>
+        <Contact informationData={informationData}/>
       </div>
     );
   }
