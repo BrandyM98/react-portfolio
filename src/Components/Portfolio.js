2a1ff8b86acfdfe722a11102f0a00d2f786a7e1a
Brandy Moore <brandymoore923@gmail.com>
2022-10-23 20:06:06 -0500

    trying to add portfolio images

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,33 +1,40 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import {BrowserRouter as Router,Link} from 'react-router-dom';
+
 export default class Porfolio extends Component {
   render() {
     let informationData = this.props.informationData;
     return (
+      <Router>
       <section id="portfolio">
-        <div className="row">
-          <div className="twelve columns collapsed">
-          <h1>Projects</h1>
-            <div class="works">
-              {informationData.portfolio &&
-                informationData.portfolio.map((item) => {
-                  return (
-                    <div className="columns portfolio-item">
-                      <div className="item-wrap">
-                        <div className="overlay">
-                            <div className="portfolio-item-meta">
-                              <h5>{item.name}</h5>
-                              <h5>{item.imgurl}</h5>
-                              <p>{item.description}</p>
-                            </div>
-                          </div>
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Work!</h1>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {
+            informationData.portfolio && informationData.portfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                      <img src={item.imgurl} alt="portfolio" className="item-img"/>
+                      <div className="overlay">
+                    <Link to={item.imglink}>
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                    </Link>
                       </div>
-                    </div>
-                  );
-                })}
-            </div>
+                  </div>
+                </div>
+              )
+            })
+          }
           </div>
         </div>
-      </section>
-    );
+      </div>
+  </section>
+  </Router>
+        );
   }
 }
--- src/information.js
+++ src/information.js
@@ -4,46 +4,6 @@ let informationData = {
   about:
     "I am a Software Engineer with a background in Biology, trained at the University of North Carolina,Charlotte, coding boot camp and earned a certificate in fullstack web development. Innovative problem-solver passionate about developing user friendly applications; focused on mobile-first design and development. Strengths in creativity, teamwork, and building projects from ideation to creation.",
   address: "Charlotte, NC",
-  education: [
-    {
-      UniversityName: "University of North Carolina, Charlotte",
-      degree: "Full stack Web Developer",
-      complete: "2022",
-    },
-    {
-      UniversityName: "Mt. San Jacinto College",
-      degree: "Associates of Art in Business Administration",
-      complete: "2020",
-    },
-    {
-      UniversityName: "California Baptist University",
-      degree: "Bachelors of Science in Biology",
-      complete: "2019",
-    },
-  ],
-  work: [
-    {
-      CompanyName: "Company",
-      title: "xxx",
-      start: "xxx",
-      finish: "xxx",
-    },
-  ],
-  skillsDescription: "Your skills here",
-  skills: [
-    {
-      skillname: "skill",
-    },
-    {
-      skillname: "skill",
-    },
-    {
-      skillname: "skill",
-    },
-    {
-      skillname: "skill",
-    },
-  ],
   portfolio: [
     {
       name: "Queen City Pups",
@@ -67,25 +27,25 @@ let informationData = {
       imgurl: "./images/queen-city.png",
     },
     {
-      name: "Queen City Pups",
+      name: "Code Quiz",
       description:
-        "Collaborated with a group of five developers to design a webpage that allows dog owners in Charlotte, NC to access local resources for their favorite pup in Queen City. This application uses Handlebars.js as a template engine, MySQL database, and Node.js.",
+        "This code quiz project is a timed multiple-choice questioned quiz. This quiz features 4 questions, a timer, and a leadership board. When you press the begin button questions will begin to populate and the timer will begin. If questions are answered incorrectly, time is deducted. This project was completed using HTML, CSS, and JavaScript.",
       website: "https://evening-river-11733.herokuapp.com/",
-      imgurl: "./images/queen-city.png",
+      imgurl: "",
     },
     {
-      name: "Tech-Blog",
+      name: "Employee Tracker",
       description:
-        "A tech-blog that allows users to create an account and post on a blog forum. Users have the ability to comment on others posts, as well as, editing and deleting their own blog posts. This application uses Handlebars.js, node js, mysql, and other technologies. ",
+        "This is a Content Management System (CMS) application that will allow you to manage a company's database. Using this application, you will be able to manage the company's employees, roles, and departments.",
       website: "https://murmuring-atoll-71811.herokuapp.com/",
-      imgurl: "./images/queen-city.png",
+      imgurl: "",
     },
     {
-      name: "Pokemon-Giphy",
+      name: "Work Day Scheduler",
       description:
-        "Collaborated with a group of four developers to design a webpage that allows Pokemon fans to search any Pokemon and receive a variety of information in regards to that said Pokemon. This application uses JavaScript, Bootstrap, jQuery, and local storage.",
+        "This project is an app that allows the user to plan their day by the hourly block from 9am-5pm. The user can save text and also clear the text when needed.",
       website: "https://bbelk.github.io/pokemon-giphy-group-project/",
-      imgurl: "./images/queen-city.png",
+      imgurl: "",
     },
   ],
 };
